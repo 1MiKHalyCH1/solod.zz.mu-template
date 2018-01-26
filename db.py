6517241f81789644ed5ab405931a7427a4932068
1MiKHalyCH1 <bykoun@gmail.com>
2018-01-26 21:10:52 +0500

    db

--- db.py
+++ db.py
@@ -15,15 +15,14 @@ class News:
 class DbNewsHandler():
     def __init__(self):
         self.SELECT_ALL = "SELECT * FROM news ORDER BY id DESC"
-        self.INSERT = "INSERT INTO news(title, body, d) VALUES ('{}','{}','{}')"
-        self.DELETE = "DELETE FROM news WHERE id={}"
+        self.INSERT = "INSERT INTO news(title, body, d) VALUES (%s,%s,%s)"
+        self.DELETE = "DELETE FROM news WHERE id=%i"
 
     @gen.coroutine
     def _start_cursor(self):
         self.conn = yield connect(**db_config)
         self.cur = self.conn.cursor()
 
-    @gen.coroutine
     def _close_cursor(self):
         self.cur.close()
         self.conn.commit()     
@@ -34,24 +33,27 @@ class DbNewsHandler():
         yield self._start_cursor()
         yield self.cur.execute(self.SELECT_ALL)
         news = [News(*e) for e in self.cur]
-        yield self._close_cursor()
-        return news
+        self._close_cursor()
+        raise gen.Return(news)
 
     @gen.coroutine
     def add_news(self, title, body):
         d = date.today().strftime('%d-%m-%y')
         yield self._start_cursor()
-        yield self.cur.execute(self.INSERT.format(title,body,d))
+        yield self.cur.execute(self.INSERT, (title,body,d))
         yield self.cur.execute(self.SELECT_ALL)
         news = [News(*e) for e in self.cur]
-        yield self._close_cursor()
-        return news 
+        self._close_cursor()
+        raise gen.Return(news)
 
     @gen.coroutine
     def delete_news(self, id):
         yield self._start_cursor()
-        yield self.cur.execute(self.DELETE.format(id))
+        try:
+            yield self.cur.execute(self.DELETE, (int(id)))
+        except Exception as  ex:
+            print(ex)
         yield self.cur.execute(self.SELECT_ALL)
         news = [News(*e) for e in self.cur]
-        yield self._close_cursor()
-        return news 
+        self._close_cursor()
+        raise gen.Return(news)
